8a865a1a15a7d42c4882fc991963f0d265ae69c1
Wonjinsin <slgktn92@gmail.com>
2021-07-03 20:43:10 +0900

    done download logic, need get input && refactoring

--- go-file-downloader/downloader/ts.go
+++ go-file-downloader/downloader/ts.go
@@ -8,10 +8,10 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type fileInfo struct {
-	lastUrl         string
 	protocol        string
 	pureUrl         string
 	format          string
@@ -20,6 +20,7 @@ type fileInfo struct {
 	fileNamePrefix  string
 	fixedFileLength int
 	lastNum         int
+	token           string
 }
 
 func Ts() {
@@ -27,34 +28,40 @@ func Ts() {
 }
 
 func fileDownload() {
-	// token := "_lsu_sa_=6705cef3b1d462e63cd6f55b69c55fb95e8a34d88d067fc83f772dc217e53225fe2fba9f61f5090700f533c285313b90e3cb7c94073af61f1fe93a53ca079792b61f166dfaedcd057a5405bba53397d0690c65db3124fbe15b1ebc19c41d056cb2097df5d273790e6c0972d3ced91d90edf7bfa1d1cbe5321cc3439b4a7bbced1dc12af7fbcee1125d0c0c2a1b918a2f"
-	// DownloadFile("repo/sample.ts", fmt.Sprintf("%s?%s", startUrl, token))
-	lastUrl := "https://b01-kr-naver-vod.pstatic.net/navertv/c/read/v2/VOD_ALPHA/navertv_2021_06_24_448/hls/86e70095-d49a-11eb-8ed1-5ebafcba569f-0000123.ts"
+	url := "https://b01-kr-naver-vod.pstatic.net/navertv/c/read/v2/VOD_ALPHA/navertv_2021_05_31_1728/hls/0d415451-c1c7-11eb-a113-d21d68c12640-000007.ts?_lsu_sa_=68955cf621876c56bada35516315a2b80edd3d08690a3fdf35a789c997d03f3567236a8867752901d02c39e215352bdc988fe4a33d47e374f47e235e996f86687f0bff63a657db97c2199e88d7a33749222d586573ab0f6adfa397816bbbdf4a0e51b3aa198af4e98b64b8ef85f0eb9f63528cb1115870abff060da53f39afa5c0c1be453ce6d9ab67c527e5d8b12d3e"
 	fileInfo := &fileInfo{}
-	fileInfo.setStruct(lastUrl)
-
-	fmt.Println(fileInfo)
+	fileInfo.setStruct(url)
 }
 
-func (f *fileInfo) setStruct(lastUrl string) {
+func (f *fileInfo) setStruct(url string) {
 
-	f.lastUrl = lastUrl
-	f.getProtocol()
+	f.getProtocol(url)
 	f.getFormat()
 	f.getPrefix()
 	f.getFilenameOnly()
 	f.getFileNamePrefix()
 
+	if f.fixedFileLength != 0 {
+		f.startIterateWithPrefix()
+	} else {
+		f.startIterate()
+	}
 }
 
-func (f *fileInfo) getProtocol() {
-	arr := strings.Split(f.lastUrl, "://")
+func (f *fileInfo) getProtocol(url string) {
+	arr := strings.Split(url, "://")
 	f.protocol = arr[0]
-	f.pureUrl = arr[1]
+
+	arr = strings.Split(arr[1], "?")
+	f.pureUrl = arr[0]
+
+	if len(arr) > 1 {
+		f.token = arr[1]
+	}
 }
 
 func (f *fileInfo) getFormat() {
-	arr := strings.Split(f.lastUrl, "/")
+	arr := strings.Split(f.pureUrl, "/")
 	fileName := arr[len(arr)-1]
 
 	arr = strings.Split(fileName, ".")
@@ -95,18 +102,82 @@ func (f *fileInfo) getFileNamePrefix() {
 			if err == nil {
 				f.fileNamePrefix = f.fileNameOnly[:i]
 				f.lastNum, _ = strconv.Atoi(f.fileNameOnly[i:])
-				break
 			}
 		}
 	}
 }
 
 func (f *fileInfo) startIterate() {
+	var wait sync.WaitGroup
+	wait.Add(f.lastNum)
+
+	for i := f.lastNum; i > 0; i-- {
+		numStr := strconv.Itoa(i)
+		str := numStr
+		result := fmt.Sprintf("%s://%s/%s%s%s?%s", f.protocol, f.prefix, f.fileNamePrefix, str, f.format, f.token)
+
+		if len(f.token) == 0 {
+			result = result[:len(result)-1]
+		}
+
+		func() {
+			defer wait.Done()
+			f.DownloadFile("repo/", result, numStr+".ts")
+		}()
+	}
+
+	wait.Wait()
+
 }
 
-func DownloadFile(filepath string, url string) error {
+func (f *fileInfo) startIterateWithPrefix() {
+	var wait sync.WaitGroup
+	wait.Add(f.lastNum)
+
+	for i := f.lastNum; i > 0; i-- {
+		numStr := strconv.Itoa(i)
+		length := len(numStr)
+		zeroCount := f.fixedFileLength - length
+		var zeroStr string
+		for i := 0; i < zeroCount; i++ {
+			zeroStr += "0"
+		}
+		str := zeroStr + numStr
+		result := fmt.Sprintf("%s://%s/%s%s%s?%s", f.protocol, f.prefix, f.fileNamePrefix, str, f.format, f.token)
+
+		if len(f.token) == 0 {
+			result = result[:len(result)-1]
+		}
+
+		go func() {
+			defer wait.Done()
+			f.DownloadFile("repo/", result, numStr+".ts")
+		}()
+	}
+
+	wait.Wait()
+}
+
+func (f *fileInfo) DownloadFile(path string, url string, filename string) error {
+	filepath := path + filename
 	// Get the data
-	resp, err := http.Get(url)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36")
+	req.Header.Add("Referer", "")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 		return err
@@ -129,7 +200,8 @@ func DownloadFile(filepath string, url string) error {
 		return err
 	}
 
-	fmt.Printf("Downloaded a file %s with size %d", filepath, size)
+	fmt.Printf("Url is %s\n", url)
+	fmt.Printf("Downloaded a file %s with size %d\n", filepath, size)
 
 	return err
 }
